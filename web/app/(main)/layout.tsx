8953fb0f8e37e5d3cbd9835e19cb19ebeb6a945a
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-10 19:13:23 +0900

    Fix: header layout

--- web/app/(main)/layout.tsx
+++ web/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="container flex h-20 mt-10 justify-between border-solid border-1 dark:border-white">
+      <header className="container flex mt-10 sm:mt-0 pt-4 pb-4 justify-between border-solid border-b border-zinc-600 dark:border-zinc-700">
         <p className="text-lg">TuneTrail</p>
         <div className="flex gap-1">
           <Icon type="search" />
