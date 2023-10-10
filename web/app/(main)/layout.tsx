f5379dd950242389ca626bc646206c781eff4ae0
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-10 18:43:52 +0900

    Add: icons

--- web/app/(main)/layout.tsx
+++ web/app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import { Icon } from "@/components/icons"
+
 interface LayoutProps {
   children: React.ReactNode
 }
@@ -5,9 +7,16 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="container">ヘッダー</header>
+      <header className="container flex h-20 mt-10 justify-between border-solid border-1 dark:border-white">
+        <p className="text-lg">TuneTrail</p>
+        <div className="flex gap-1">
+          <Icon type="search" />
+          <Icon type="notify" />
+          <Icon type="user" />
+          <Icon type="new-post" />
+        </div>
+      </header>
       <main className="container flex-1">{children}</main>
-      {/* <footer className="container">フッター</footer> */}
     </div>
   )
 }
