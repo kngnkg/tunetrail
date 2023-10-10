6cc97575a72818236881a63d013556d1f25018f5
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-10 14:32:57 +0900

    Installed shadcn/ui

--- web/app/(main)/page.tsx
+++ web/app/(main)/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link"
 
+import { Button } from "@/components/ui/button"
+
 export default function IndexPage() {
   return (
     <>
       <section>
         <p>Home Page</p>
+        <Button>Click me</Button>
       </section>
     </>
   )
