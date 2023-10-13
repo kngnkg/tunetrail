24dcbb579b641e8fc6a98c163f0e70be8ff08eb2
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-13 13:31:20 +0900

    Add: user page

--- web/app/(main)/layout.tsx
+++ web/app/(main)/layout.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link"
-
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { MainNav } from "@/components/main-nav"
+import { MenuTab, MenuTabs } from "@/components/menu-tabs"
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 export default function Layout({ children }: LayoutProps) {
-  const tabsTriggerClassName =
-    "data-[state=active]:border-b-4 border-primary dark:border-primary rounded-none focus-visible:ring-none"
+  const tabs: MenuTab[] = [
+    { label: "ホーム", value: "home", href: "/" },
+    { label: "タイムライン", value: "timeline", href: "/timeline" },
+  ]
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -17,20 +17,7 @@ export default function Layout({ children }: LayoutProps) {
         <MainNav />
       </header>
       <main className="container flex-1">
-        <Tabs defaultValue="home" className="sm:w-4/6 mx-auto">
-          <TabsList className="bg-transparent dark:bg-transparent">
-            <Link href="/">
-              <TabsTrigger value="home" className={tabsTriggerClassName}>
-                ホーム
-              </TabsTrigger>
-            </Link>
-            <Link href="/timeline">
-              <TabsTrigger value="timeline" className={tabsTriggerClassName}>
-                タイムライン
-              </TabsTrigger>
-            </Link>
-          </TabsList>
-        </Tabs>
+        <MenuTabs tabs={tabs} />
         <div className="border-solid border-b border-zinc-700 dark:border-zinc-700" />
         <div className="sm:w-4/6 mx-auto">{children}</div>
       </main>
