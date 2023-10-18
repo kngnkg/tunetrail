a5fbf2d4d4f03e449effad43a15d5ef48d3f0a51
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-18 10:45:01 +0900

    Add: sample server

--- api/main.go
+++ api/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	helloworld "github.com/kngnkg/tunetrail/api/gen/go/hello_world"
+	"github.com/kngnkg/tunetrail/api/infra/server"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
-	println("Hello World")
+	s := grpc.NewServer()
+
+	helloworldServer := server.NewHelloworldServer()
+	helloworld.RegisterGreeterServer(s, helloworldServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Println("gRPC server started on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
