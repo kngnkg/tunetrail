3a9ef5db3aa490d6530a49ea45718258abc49780
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-18 20:52:48 +0900

    Add: GetByAuthorId

--- api/usecase/review.go
+++ api/usecase/review.go
@@ -4,23 +4,26 @@ import (
 	"context"
 
 	"github.com/kngnkg/tunetrail/api/entity"
+	"golang.org/x/sync/errgroup"
 )
 
 //go:generate go run github.com/matryer/moq -out moq_test.go . ReviewRepository AlbumRepository UserRepository
 type ReviewRepository interface {
 	Store(ctx context.Context, review *entity.Review) (*entity.Review, error)
 	GetById(ctx context.Context, reviewId string) (*entity.Review, error)
+	GetByAuthorId(ctx context.Context, authorId entity.UserId, nextCursor string, limit int) ([]*entity.Review, string, error)
 }
 
 type AlbumRepository interface {
 	GetById(ctx context.Context, albumId string) (*entity.Album, error)
+	GetByIds(ctx context.Context, albumIds []string) ([]*entity.Album, error)
 }
 
 type UserRepository interface {
 	GetById(ctx context.Context, userId entity.UserId) (*entity.User, error)
 }
 
-//go:generate go run github.com/cweill/gotests/... -all -w review_test.go review.go
+//go:generate go run github.com/cweill/gotests/... -exported -w review_test.go review.go
 type ReviewUseCase struct {
 	reviewRepo ReviewRepository
 	albumRepo  AlbumRepository
@@ -69,4 +72,76 @@ func (uc *ReviewUseCase) GetById(ctx context.Context, reviewId string) (*entity.
 
 	r.Album = album
 	return r, nil
+}
+
+func (uc *ReviewUseCase) GetByAuthorId(ctx context.Context, authorId entity.UserId, nextCursor string, limit int) ([]*entity.Review, string, error) {
+	rs, nc, err := uc.reviewRepo.GetByAuthorId(ctx, authorId, nextCursor, limit)
+	if err != nil {
+		return nil, "", err
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// アルバム情報を取得
+	eg.Go(func() error {
+		aIds := make([]string, len(rs))
+		for i, r := range rs {
+			aIds[i] = r.Album.AlbumId
+		}
+
+		as, err := uc.albumRepo.GetByIds(ctx, aIds)
+		if err != nil {
+			return err
+		}
+
+		for _, r := range rs {
+			for _, a := range as {
+				if r.Album.AlbumId == a.AlbumId {
+					r.Album = a
+				}
+			}
+		}
+
+		return nil
+	})
+
+	// ユーザー情報を取得
+	eg.Go(func() error {
+		rs, err = uc.fillUserInfo(ctx, rs)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err := eg.Wait(); err != nil {
+		return nil, "", err
+	}
+
+	return rs, nc, nil
+}
+
+func (uc *ReviewUseCase) fillUserInfo(ctx context.Context, reviews []*entity.Review) ([]*entity.Review, error) {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for _, r := range reviews {
+		r := r
+		eg.Go(func() error {
+			author, err := uc.userRepo.GetById(ctx, r.Author.UserId)
+			if err != nil {
+				return err
+			}
+
+			r.Author = author
+
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+
+	return reviews, nil
 }
--- api/usecase/review_test.go
+++ api/usecase/review_test.go
@@ -6,9 +6,21 @@ import (
 	"testing"
 
 	"github.com/kngnkg/tunetrail/api/entity"
+	"github.com/kngnkg/tunetrail/api/testutil"
 	"github.com/kngnkg/tunetrail/api/testutil/fixture"
 	"github.com/stretchr/testify/assert"
 )
+
+func prepareReviews(t *testing.T) []*entity.Review {
+	t.Helper()
+
+	var wants []*entity.Review
+	for i := 0; i < 10; i++ {
+		wants = append(wants, fixture.Review(nil))
+	}
+
+	return wants
+}
 
 func TestReviewUseCase_Store(t *testing.T) {
 	t.Parallel()
@@ -132,6 +144,79 @@ func TestReviewUseCase_GetById(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ReviewUseCase.GetById() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestReviewUseCase_GetByAuthorId(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		ctx        context.Context
+		authorId   entity.UserId
+		nextCursor string
+		limit      int
+	}
+	tests := []struct {
+		name       string
+		args       args
+		want       []*entity.Review
+		wantCursor string
+		wantErr    bool
+	}{
+		{
+			name: "正常系",
+			args: args{
+				ctx:        context.Background(),
+				authorId:   "authorId",
+				nextCursor: "",
+				limit:      10,
+			},
+			want:       prepareReviews(t),
+			wantCursor: testutil.GenRandomUUID(),
+			wantErr:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			moqReviewRepo := &ReviewRepositoryMock{}
+			moqAlbumRepo := &AlbumRepositoryMock{}
+			moqUserRepo := &UserRepositoryMock{}
+			moqReviewRepo.GetByAuthorIdFunc = func(pctx context.Context, authorId entity.UserId, nextCursor string, limit int) ([]*entity.Review, string, error) {
+				return tt.want, tt.wantCursor, nil
+			}
+			moqAlbumRepo.GetByIdsFunc = func(pctx context.Context, albumIds []string) ([]*entity.Album, error) {
+				var as []*entity.Album
+				for _, r := range tt.want {
+					as = append(as, r.Album)
+				}
+				return as, nil
+			}
+			moqUserRepo.GetByIdFunc = func(pctx context.Context, userId entity.UserId) (*entity.User, error) {
+				for _, r := range tt.want {
+					if r.Author.UserId == userId {
+						return r.Author, nil
+					}
+				}
+				return nil, nil
+			}
+
+			uc := &ReviewUseCase{
+				reviewRepo: moqReviewRepo,
+				albumRepo:  moqAlbumRepo,
+				userRepo:   moqUserRepo,
+			}
+			got, gotCursor, err := uc.GetByAuthorId(tt.args.ctx, tt.args.authorId, tt.args.nextCursor, tt.args.limit)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReviewUseCase.GetByAuthorId() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReviewUseCase.GetByAuthorId() got = %v, want %v", got, tt.want)
+			}
+			if gotCursor != tt.wantCursor {
+				t.Errorf("ReviewUseCase.GetByAuthorId() gotCursor = %v, want %v", gotCursor, tt.wantCursor)
 			}
 		})
 	}
