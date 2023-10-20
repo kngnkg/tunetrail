4b57fd6227efaab0b5842cd5a01795e74f48d5f1
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-20 19:37:20 +0900

    Add: env setting for test

---
+++ frontend/setupEnv.js
@@ -0,0 +1,6 @@
+module.exports = () => {
+  process.env.API_ROOT = "http://api.example.com"
+  process.env.GRPC_API_ROOT = "grpc://api.example.com"
+  process.env.NEXT_PUBLIC_API_ROOT = "http://api.example.com"
+  process.env.NEXT_PUBLIC_SPOTIFY_CDN_HOST = "http://cdn.example.com"
+}
