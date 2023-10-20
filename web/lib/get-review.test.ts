a3e86215cc866da24b213408284717b1c27b1cb2
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-20 18:38:34 +0900

    setup jest

---
+++ web/lib/get-review.test.ts
@@ -0,0 +1,26 @@
+import { env } from "@/env.mjs"
+
+import { getReview } from "./get-review"
+
+describe("getReview", () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it("test", async () => {
+    // const mockData = {
+    //   // モックデータの中身を適切な形に設定する
+    // }
+    // const transformedMockData = {
+    //   // transformReviewによって変換される想定のデータを設定する
+    // }
+
+    // global.fetch = jest.fn().mockResolvedValue({ mockData })
+
+    // const result = await getReview(env.API_ROOT)
+
+    // expect(result).toEqual(transformedMockData)
+
+    console.log("test")
+  })
+})
