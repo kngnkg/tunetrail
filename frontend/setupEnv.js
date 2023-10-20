a384d22fc42bd75d9cba6e18d21ab9f5237b13b5
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-20 20:15:49 +0900

    テスト用の環境変数の設定を追加

--- frontend/setupEnv.js
+++
@@ -1,6 +0,0 @@
-module.exports = () => {
-  process.env.API_ROOT = "http://api.example.com"
-  process.env.GRPC_API_ROOT = "grpc://api.example.com"
-  process.env.NEXT_PUBLIC_API_ROOT = "http://api.example.com"
-  process.env.NEXT_PUBLIC_SPOTIFY_CDN_HOST = "http://cdn.example.com"
-}
