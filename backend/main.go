90cee0a9ef75d4ae2620b13773c5b74978628268
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-22 12:35:58 +0900

    Add: config

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net"
+	"strconv"
 
+	"github.com/kngnkg/tunetrail/backend/config"
 	"github.com/kngnkg/tunetrail/backend/infra/server"
 	"github.com/kngnkg/tunetrail/backend/logger"
 
@@ -17,6 +20,13 @@ func main() {
 		Level: slog.LevelDebug,
 	})
 
+	cfg, err := config.New()
+	if err != nil {
+		l.Fatal("failed to load config.", err)
+	}
+
+	// TODO: usecaseなどのインスタンスを生成する
+
 	s := grpc.NewServer()
 
 	helloworldServer := server.NewHelloworldServer()
@@ -25,12 +35,12 @@ func main() {
 	userServer := server.NewUserServer()
 	user.RegisterUserServiceServer(s, userServer)
 
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", cfg.Port))
 	if err != nil {
 		l.Fatal("failed to listen.", err)
 	}
 
-	l.Info("gRPC server started.", "port", "50051")
+	l.Info("gRPC server started.", "port", strconv.Itoa(cfg.Port))
 	if err := s.Serve(lis); err != nil {
 		l.Fatal("failed to serve.", err)
 	}
