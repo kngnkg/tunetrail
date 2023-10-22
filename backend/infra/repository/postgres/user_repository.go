0df1b63ac98e798502a8d7fe098fe719155b41c4
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-22 18:13:15 +0900

    ユーザー登録機能を追加

--- backend/infra/repository/postgres/user_repository.go
+++ backend/infra/repository/postgres/user_repository.go
@@ -9,6 +9,20 @@ import (
 
 type UserRepository struct{}
 
+func (r *UserRepository) StoreUser(ctx context.Context, db repository.Executor, user *entity.User) (*entity.User, error) {
+	query := `
+		INSERT INTO users (user_id, display_id, name, avatar_url, bio, created_at, updated_at)
+		VALUES (:user_id, :display_id, :name, :avatar_url, :bio, NOW(), NOW())
+		RETURNING user_id, display_id, name, avatar_url, bio, created_at, updated_at;`
+
+	_, err := db.NamedExecContext(ctx, query, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
 func (r *UserRepository) GetUserById(ctx context.Context, db repository.Executor, userId entity.UserId) (*entity.User, error) {
 	u := &entity.User{}
 
