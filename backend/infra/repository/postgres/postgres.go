d3136cd3be56568ea5fc88a96676bd8a83c5c308
kngnkg <kawata.ryo.0518@gmail.com>
2023-10-23 12:36:15 +0900

    Add: DB config

--- backend/infra/repository/postgres/postgres.go
+++ backend/infra/repository/postgres/postgres.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
-	"github.com/kngnkg/tunetrail/backend/config"
 	"github.com/kngnkg/tunetrail/backend/infra/repository"
 	_ "github.com/lib/pq"
 )
@@ -15,15 +14,19 @@ type DB struct {
 	*sqlx.DB
 }
 
-func NewDb(cfg *config.Config) (*DB, func(), error) {
-	sslMode := "require"
-	if cfg.Env == "dev" {
-		sslMode = "disable" // 開発環境の場合はSSL通信を無効にする
-	}
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
 
+func NewDB(cfg *DBConfig) (*DB, func(), error) {
 	db, err := sqlx.Connect("postgres", fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
-		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, sslMode,
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode,
 	))
 	if err != nil {
 		return nil, func() {}, err
