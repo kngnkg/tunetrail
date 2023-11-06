3bfff58345bc4b6cbca71eae10c97404c1dcd15e
kngnkg <kawata.ryo.0518@gmail.com>
2023-11-06 10:47:57 +0900

    Session から LoginUser を取得する処理を関数に切り出し

--- frontend/app/(auth)/welcome/page.tsx
+++ frontend/app/(auth)/welcome/page.tsx
@@ -1,10 +1,6 @@
-import { notFound, redirect } from "next/navigation"
-import { toUser } from "@/service/transform"
-import getMe from "@/service/user/get-me"
-import { LoginUser } from "@/types"
-import { getServerSession } from "next-auth"
+import { notFound } from "next/navigation"
 
-import { authOptions } from "@/lib/auth-options"
+import { getCurrentUser } from "@/lib/session"
 import { WelcomeForm } from "@/components/users/welcome-form"
 
 export type Phase = "username" | "profile" | "complete"
@@ -17,32 +13,13 @@ interface WelcomePageProps {
   searchParams: { [key: string]: Phase | undefined }
 }
 
-const getCurrentUser = async (idToken: string): Promise<LoginUser | null> => {
-  try {
-    const resp = await getMe(idToken)
-    if (!resp) {
-      return null
-    }
-
-    return toUser(resp)
-  } catch (e) {
-    console.error(e)
-    return null
-  }
-}
-
 export default async function WelcomePage({ searchParams }: WelcomePageProps) {
   const phase = searchParams.phase || "username"
   if (!isPhase(phase)) {
     return notFound()
   }
 
-  const session = await getServerSession(authOptions)
-  if (!session || !session.idToken) {
-    return redirect("/")
-  }
-
-  const user = await getCurrentUser(session.idToken)
+  const user = await getCurrentUser()
   if (!user) {
     return <p>Something went wrong.</p>
   }
