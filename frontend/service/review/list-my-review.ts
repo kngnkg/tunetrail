423401900ce7b7f10c2f618b870a779e8c8eaf6e
kngnkg <kawata.ryo.0518@gmail.com>
2023-11-08 07:13:23 +0000

    認証済みユーザーのレビュー一覧取得処理を追加

---
+++ frontend/service/review/list-my-review.ts
@@ -0,0 +1,41 @@
+import {
+  ListReviewsRequest,
+  Review,
+  ReviewList,
+} from "@/generated/review/review_pb"
+
+import { getMetadata } from "@/lib/grpc"
+
+import { client } from "./client"
+
+export interface ListMyReviewsParams {
+  cursor: string | null
+  limit: number | null
+}
+
+export default function listMyReviews(
+  idToken: string,
+  params: ListMyReviewsParams
+): Promise<ReviewList | null> {
+  return new Promise((resolve, reject) => {
+    const { cursor, limit } = params
+
+    const req = new ListReviewsRequest()
+
+    if (cursor && cursor !== "") {
+      req.setCursor(cursor)
+    }
+
+    if (limit && limit > 0) {
+      req.setLimit(limit)
+    }
+
+    const metadata = getMetadata(idToken)
+    client.listMyReviews(req, metadata, (err, response) => {
+      if (err) reject(err)
+      if (!response) return resolve(null)
+
+      resolve(response)
+    })
+  })
+}
