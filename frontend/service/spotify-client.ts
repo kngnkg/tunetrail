26ed800a60ce95310eb0764b7948e1b8a6b8ce15
kngnkg <kawata.ryo.0518@gmail.com>
2023-11-09 03:04:24 +0000

    Redis の設定を追加

--- frontend/service/spotify-client.ts
+++ frontend/service/spotify-client.ts
@@ -1,18 +1,32 @@
+import { Redis } from "ioredis"
 import SpotifyWebApi from "spotify-web-api-node"
 
 import { env } from "@/env.mjs"
+import { setAccessTokenToKVS } from "@/lib/kvs"
 
 // export const client = new SpotifyWebApi({
 //   clientId: env.SPOTIFY_CLIENT_ID,
 //   clientSecret: env.SPOTIFY_CLIENT_SECRET,
 // })
 
-// // アクセストークンを取得する
-// client.clientCredentialsGrant().then(
-//   function (data) {
-//     client.setAccessToken(data.body["access_token"])
-//   },
-//   function (err) {
-//     console.log("Something went wrong when retrieving an access token", err)
-//   }
-// )
+export const clientCredentialsGrant = async (
+  redisClient: Redis
+): Promise<string | null> => {
+  try {
+    // アクセストークンを取得する
+    // const data = await client.clientCredentialsGrant()
+    // const token = data.body["access_token"]
+    // const expiresInSeconds = data.body["expires_in"]
+
+    const token = "test"
+    const expiresInSeconds = 3600
+
+    // KVSに保存する
+    setAccessTokenToKVS(redisClient, { token, expiresIn: expiresInSeconds })
+
+    return token
+  } catch (e) {
+    console.error(e)
+    throw new Error("Failed to fetch token")
+  }
+}
