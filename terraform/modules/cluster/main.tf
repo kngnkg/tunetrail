aecd799f539c1d309e124362cc5f6eee6733bb71
kngnkg <kawata.ryo.0518@gmail.com>
2023-11-12 17:26:41 +0900

    ECS タスク実行ロールを追加

--- terraform/modules/cluster/main.tf
+++ terraform/modules/cluster/main.tf
@@ -11,3 +11,35 @@ resource "aws_ecs_cluster" "this" {
     Name = "tunetrail-${var.env}-cluster"
   }
 }
+
+# ECS タスク実行ロール
+resource "aws_iam_role" "task_executer" {
+  name        = "tunetrail${title(var.env)}EcsTaskExecutionRole"
+  description = "Allows ECS tasks to call AWS services on your behalf."
+
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "ecs-tasks.amazonaws.com"
+          }
+          Sid = ""
+        }
+      ]
+      Version = "2012-10-17"
+    }
+  )
+
+  tags = {
+    "Name" = "tunetrail-${var.env}-ecs-task-execution-role"
+  }
+}
+
+# ECS タスク実行ロールにアタッチするポリシー
+resource "aws_iam_role_policy_attachment" "allow_ecs_task_execution_role" {
+  role       = aws_iam_role.task_executer.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
--- terraform/modules/cluster/outputs.tf
+++ terraform/modules/cluster/outputs.tf
@@ -1,3 +1,7 @@
 output "id" {
   value = aws_ecs_cluster.this.id
 }
+
+output "task_execution_role_arn" {
+  value = aws_iam_role.task_executer.arn
+}
