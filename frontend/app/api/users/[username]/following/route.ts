94ba5593a00638a1ffc2c525d5183bab9eda0d5d
kngnkg <kawata.ryo.0518@gmail.com>
2023-11-28 19:57:35 +0900

    フォロー取得エンドポイントを追加

---
+++ frontend/app/api/users/[username]/following/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server"
+import listFollows from "@/service/follow/list-follows"
+
+import { getServerSession } from "@/lib/session"
+import { errBadRequest, errInternal, errUnauthorized } from "@/app/api/response"
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+
+  const usernamesStr = searchParams.get("target_usernames")
+  if (!usernamesStr) {
+    return errBadRequest("invalid request")
+  }
+
+  // カンマ区切りの文字列を配列に変換
+  const usernames = usernamesStr.split(",")
+
+  try {
+    const session = await getServerSession()
+    if (!session || !session.idToken) {
+      return errUnauthorized("unauthorized")
+    }
+
+    const resp = await listFollows(session.idToken, usernames)
+    if (!resp) {
+      return errInternal("internal error")
+    }
+
+    return NextResponse.json({
+      follows: resp.map((follow) => ({
+        username: follow.getUsername(),
+        immutableId: follow.getImmutableid(),
+        displayName: follow.getDisplayname(),
+        isFollowing: follow.getIsfollowing(),
+      })),
+    })
+  } catch (e) {
+    console.error(e)
+    return errInternal("internal error")
+  }
+}
+
+// POST
---
+++ frontend/service/follow/list-follows.ts
@@ -0,0 +1,28 @@
+import {
+  FollowResponse,
+  ListFollowsRequest,
+} from "@/generated/follow/follow_pb"
+
+import { getMetadata } from "@/lib/grpc"
+
+import { client } from "./client"
+
+export default function listFollows(
+  idToken: string,
+  usernames: string[]
+): Promise<FollowResponse[] | null> {
+  return new Promise((resolve, reject) => {
+    const req = new ListFollowsRequest()
+
+    req.setUsernamesList(usernames)
+
+    const metadata = getMetadata(idToken)
+    client.listFollows(req, metadata, (err, response) => {
+      if (err) {
+        return reject(err)
+      }
+      if (!response) return resolve(null)
+      resolve(response.getFollowresponsesList())
+    })
+  })
+}
