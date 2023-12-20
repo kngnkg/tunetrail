dc6ccf733d55be7010e9aa1878084d93d5d81345
kngnkg <kawata.ryo.0518@gmail.com>
2023-12-20 19:29:17 +0900

    フォロワー取得機能を追加

---
+++ frontend/service/follow/list-followers.ts
@@ -0,0 +1,39 @@
+import { ListFollowingsRequest } from "@/generated/follow_pb"
+import { ListUsersRequest, UserList } from "@/generated/user_pb"
+import { ListUsersParams } from "@/types"
+
+import { client } from "./client"
+
+export default function listFollowers(
+  username: string,
+  params: ListUsersParams // TODO:別の場所に定義
+): Promise<UserList | null> {
+  return new Promise((resolve, reject) => {
+    const { cursor, limit } = params
+
+    const req = new ListFollowingsRequest()
+    req.setUsername(username)
+
+    const pagenation = new ListUsersRequest()
+
+    if (cursor && cursor !== "") {
+      pagenation.setCursor(cursor)
+    }
+
+    if (limit && limit > 0) {
+      pagenation.setLimit(limit)
+    }
+
+    if (pagenation !== null) {
+      req.setPagenation(pagenation)
+    }
+
+    client.listFollowers(req, (err, response) => {
+      if (err) {
+        return reject(err)
+      }
+      if (!response) return resolve(null)
+      resolve(response)
+    })
+  })
+}
