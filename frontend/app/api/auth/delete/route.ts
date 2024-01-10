8849352a41e523f3d757d375095ac107300aef99
kngnkg <kawata.ryo.0518@gmail.com>
2024-01-10 22:30:45 +0900

    aws-sdkを追加

--- frontend/app/api/auth/delete/route.ts
+++ frontend/app/api/auth/delete/route.ts
@@ -1,7 +1,11 @@
 import { NextRequest, NextResponse } from "next/server"
+import CognitoIdentityServiceProvider, * as CognitoIdp from "aws-sdk/clients/cognitoidentityserviceprovider"
 
 export async function POST(request: NextRequest) {
   try {
+    const cognitoIdpClient = new CognitoIdentityServiceProvider({
+      region: "ap-northeast-1", // TODO: 環境変数から取得する
+    })
   } catch (e) {
     console.error(e)
     return errInternal("internal error")
