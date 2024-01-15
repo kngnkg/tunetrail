954c1a481647e3f4e0e99c1d4b2236226a26684b
kngnkg <kawata.ryo.0518@gmail.com>
2024-01-15 21:42:27 +0900

    環境変数を追加

--- frontend/app/api/auth/delete/route.ts
+++ frontend/app/api/auth/delete/route.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from "next/server"
 import CognitoIdentityServiceProvider, * as CognitoIdp from "aws-sdk/clients/cognitoidentityserviceprovider"
 
+import { env } from "@/env.mjs"
+
 export async function POST(request: NextRequest) {
   try {
     const cognitoIdpClient = new CognitoIdentityServiceProvider({
-      region: "ap-northeast-1", // TODO: 環境変数から取得する
+      region: env.AWS_REGION,
     })
   } catch (e) {
     console.error(e)
