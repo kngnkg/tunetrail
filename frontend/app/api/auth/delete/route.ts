2d665d271a809bb410dbf7a89ff52652b14ff84d
kngnkg <kawata.ryo.0518@gmail.com>
2024-01-18 22:49:37 +0900

    削除処理を追加

--- frontend/app/api/auth/delete/route.ts
+++ frontend/app/api/auth/delete/route.ts
@@ -1,13 +1,34 @@
 import { NextRequest, NextResponse } from "next/server"
-import CognitoIdentityServiceProvider, * as CognitoIdp from "aws-sdk/clients/cognitoidentityserviceprovider"
+import {
+  AdminDeleteUserCommand,
+  CognitoIdentityProviderClient,
+} from "@aws-sdk/client-cognito-identity-provider"
 
 import { env } from "@/env.mjs"
 
+import { errInternal } from "../../response"
+
 export async function POST(request: NextRequest) {
   try {
-    const cognitoIdpClient = new CognitoIdentityServiceProvider({
+    const client = new CognitoIdentityProviderClient({
       region: env.AWS_REGION,
     })
+
+    const deleteUser = async (username: string, userPoolId: string) => {
+      const command = new AdminDeleteUserCommand({
+        UserPoolId: userPoolId,
+        Username: username,
+      })
+
+      try {
+        // コマンドの実行
+        const response = await client.send(command)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    deleteUser("exampleUsername", "yourUserPoolId")
   } catch (e) {
     console.error(e)
     return errInternal("internal error")
